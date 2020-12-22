f589034b661096c6fa8c9299775b4287a70bf548
VolodSh <vzujka.com.ua@gmail.com>
2020-12-22 21:20:48 +0200

    Updates

---
+++ precache-manifest.b60ab275dc89a9d1aa9dc4cd0135d274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63b9d1428a26f83241d6a5a6e67d452",
+    "url": "/react_picture_finder/index.html"
+  },
+  {
+    "revision": "ae0301a4cbb87b0dc8b4",
+    "url": "/react_picture_finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "f760ac341db6744cd703",
+    "url": "/react_picture_finder/static/css/main.9273f663.chunk.css"
+  },
+  {
+    "revision": "ae0301a4cbb87b0dc8b4",
+    "url": "/react_picture_finder/static/js/2.5cacb784.chunk.js"
+  },
+  {
+    "revision": "f760ac341db6744cd703",
+    "url": "/react_picture_finder/static/js/main.6f594bcd.chunk.js"
+  },
+  {
+    "revision": "1edbfe2a1e5e37856929",
+    "url": "/react_picture_finder/static/js/runtime-main.8428e2dc.js"
+  }
+]);
